99c5d42f45b0276000e5211fba0b05766431afcc
kaua <kauavbrandaorocha@gmail.com>
2025-09-18 19:07:04 -0300

    fix | Corrigir erro de sintaxe e erro de declaração dos serializadores

--- api_cidade/cidades/api/serializers.py
+++ api_cidade/cidades/api/serializers.py
@@ -5,16 +5,18 @@ class RegiaoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Regiao
         fields = ['id', 'nome', 'sigla']
-        ready_only_fields = ['nome', 'sigla']
+        read_only_fields = ['nome', 'sigla']
 
 class UFSerializer(serializers.ModelSerializer):
+    regiao = RegiaoSerializer()
     class Meta:
-        regiao = RegiaoSerializer()
         fields = ['id', 'nome', 'sigla', 'regiao']
-        ready_only_fields = ['nome', 'sigla', 'regiao']
+        read_only_fields = ['nome', 'sigla', 'regiao']
 
 class MunicipioSerializer(serializers.ModelSerializer):
+    uf = UFSerializer()
+
     class Meta:
         uf = UFSerializer()
         fields = ['id', 'nome', 'uf']
-        ready_only_fields = ['nome', 'uf']
+        read_only_fields = ['nome', 'uf']
