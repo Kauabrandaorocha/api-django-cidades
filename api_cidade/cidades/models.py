567cb0e034e9f1c4eb6c0c17fdbfa80ed9a5ef26
kaua <kauavbrandaorocha@gmail.com>
2025-09-18 15:45:59 -0300

    feat | Criação da model

--- api_cidade/cidades/models.py
+++ api_cidade/cidades/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Regiao(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nome = models.CharField(max_length=100, unique=True, null=False)
+    sigla = models.CharField(max_length=2, unique=True, null=False, verbose_name="Sigla da Região")
+
+
+class UF(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nome = models.CharField(max_length=100, unique=True, null=False)
+    sigla = models.CharField(max_length=2, unique=True, null=False, verbose_name="Sigla da Unidade da Federação")
+    regiao = models.ForeignKey(Regiao)
+
+class Municipio(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nome = models.CharField(max_length=100, unique=True)
+    uf = models.ForeignKey(UF)
