6dad44d4d333f83312da8030744ccf3cf83b68d2
kaua <kauavbrandaorocha@gmail.com>
2025-09-18 16:15:38 -0300

    feat | adiciona def __str__ para representação legível

---
+++ api_cidade/cidades/api/serializers.py
@@ -0,0 +1,3 @@
+from rest_framework import serializers
+from cidades.models import Municipio
+
--- api_cidade/cidades/models.py
+++ api_cidade/cidades/models.py
@@ -6,6 +6,9 @@ class Regiao(models.Model):
     nome = models.CharField(max_length=100, unique=True, null=False)
     sigla = models.CharField(max_length=2, unique=True, null=False, verbose_name="Sigla da Região")
 
+    def __str__(self):
+        return f"{self.nome} ({self.sigla})"
+
 
 class UF(models.Model):
     id = models.IntegerField(primary_key=True)
@@ -13,7 +16,13 @@ class UF(models.Model):
     sigla = models.CharField(max_length=2, unique=True, null=False, verbose_name="Sigla da Unidade da Federação")
     regiao = models.ForeignKey(Regiao, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.nome} ({self.sigla})"
+
 class Municipio(models.Model):
     id = models.IntegerField(primary_key=True)
     nome = models.CharField(max_length=100, unique=True)
     uf = models.ForeignKey(UF, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.nome} - {self.uf.sigla}"
