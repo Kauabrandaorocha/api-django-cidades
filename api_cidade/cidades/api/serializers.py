635994a1c9440624f31faa6cdf9d4c423c9d7eb8
kaua <kauavbrandaorocha@gmail.com>
2025-09-18 16:25:36 -0300

    feat | Cria serializers para validar e traduzir objetos

--- api_cidade/cidades/api/serializers.py
+++ api_cidade/cidades/api/serializers.py
@@ -1,3 +1,20 @@
 from rest_framework import serializers
-from cidades.models import Municipio
+from cidades.models import Municipio, Regiao, UF
 
+class RegiaoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Regiao
+        fields = ['id', 'nome', 'sigla']
+        ready_only_fields = ['nome', 'sigla']
+
+class UFSerializer(serializers.ModelSerializer):
+    class Meta:
+        regiao = RegiaoSerializer()
+        fields = ['id', 'nome', 'sigla', 'regiao']
+        ready_only_fields = ['nome', 'sigla', 'regiao']
+
+class MunicipioSerializer(serializers.ModelSerializer):
+    class Meta:
+        uf = UFSerializer()
+        fields = ['id', 'nome', 'uf']
+        ready_only_fields = ['nome', 'uf']
